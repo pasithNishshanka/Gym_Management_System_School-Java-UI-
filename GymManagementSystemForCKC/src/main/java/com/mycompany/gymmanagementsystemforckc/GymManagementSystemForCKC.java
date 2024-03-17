c4a60095c703c85ec3d2bb7c147427051f804686
I Pasith Nishshanka <137369620+pasithNishshanka@users.noreply.github.com>
2024-03-17 08:02:29 +0530

    Add files via upload

---
+++ GymManagementSystemForCKC/src/main/java/com/mycompany/gymmanagementsystemforckc/GymManagementSystemForCKC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gymmanagementsystemforckc;
+
+/**
+ *
+ * @author DELL
+ */
+public class GymManagementSystemForCKC {
+
+    public static void main(String[] args) {
+        login loginFrame= new login();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+
+    }
+}
